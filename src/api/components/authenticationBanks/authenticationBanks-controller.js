654c97f4f3ec6a03aaf2b23fd0828461d4dcf17d
wei5002 <shirleywei08@gmail.com>
2024-05-01 23:14:18 +0700

    sesuai syarat

--- src/api/components/authenticationBanks/authenticationBanks-controller.js
+++ src/api/components/authenticationBanks/authenticationBanks-controller.js
@@ -1,10 +1,6 @@
 const { errorResponder, errorTypes } = require('../../../core/errors');
 const authenticationServices = require('./authenticationBanks-service');
 
-const jumlahGagal ={ }; 
-// jumlahGagal --> untuk menampung berapa gagal yang bakal ada saat ketika kita login, 
-// atau berapa percobaan saat kita login
-
 /**
  * Handle login request
  * @param {object} request - Express request object
@@ -16,20 +12,6 @@ async function login(request, response, next) {
   const { email, password } = request.body;
 
   try {
-    if (!jumlahGagal[email]){
-      jumlahGagal[email]={
-        awalLogin:0,
-        akhirLogin: null,
-      };
-    }
-
-    if (jumlahGagal[email].awalLogin>= 5 && Date.now()- jumlahGagal[email].akhirLogin < 60000){
-      throw errorResponder(
-        errorTypes.FORBIDDEN,
-        'Too many failed login attempts.'
-      );
-    }
-
     // Check login credentials
     const loginSuccess = await authenticationServices.checkLoginCredentials(
       email,
@@ -37,17 +19,12 @@ async function login(request, response, next) {
     );
 
     if (loginSuccess) {
-      jumlahGagal[email].awalLogin = 0;
-      jumlahGagal[email].akhirLogin= null;
-    return response.status(200).json(loginSuccess);      
+      throw errorResponder(
+        errorTypes.INVALID_CREDENTIALS,
+        'Kesalahan email atau password'
+      );
     }
-    
-    jumlahGagal[email].awalLogin += 1;
-    jumlahGagal[email].akhirLogin= Date.now();
-    throw errorResponder(
-      errorTypes.INVALID_CREDENTIALS,
-      'Kesalahan email atau password'
-    );
+    return response.status(200).json(loginSuccess);
   } catch (error) {
     return next(error);
   }
